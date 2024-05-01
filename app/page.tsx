db23db841ca424dc4c2426a8913918f4678b6817
Sergio Silva <sergiobtos@hotmail.com>
2024-05-01 15:20:27 -0400

    feat: add product list and product item component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,29 @@
+import { ChevronsRightIcon } from "lucide-react";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
+import ProductList from "./_components/product-list";
+import PromoBanner from "./_components/promo-banner";
 import Search from "./_components/search";
+import { Button } from "./_components/ui/button";
+import { db } from "./_lib/prisma";
 
-const Home = () => {
+const Home = async () => {
+  const products = await db.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+    take: 10,
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
+
   return (
     <>
       <Header />
@@ -12,6 +33,27 @@ const Home = () => {
 
       <div className="pt-6">
         <CategoryList />
+      </div>
+
+      <div className="px-5 pt-6">
+        <PromoBanner
+          src="/promo-banner-01.png"
+          alt="Até 30% de desconto em pizzas!"
+        />
+      </div>
+
+      <div className="space-y-3 pt-6">
+        <div className="flex items-center justify-between px-5">
+          <h2 className="font-semibold">Pedidos Recomendados</h2>
+          <Button
+            className="h-fit p-0 text-primary hover:bg-transparent"
+            variant="ghost"
+          >
+            Ver todos
+            <ChevronsRightIcon size={16} />
+          </Button>
+        </div>
+        <ProductList products={products} />
       </div>
     </>
   );
