a04255f97f9ddf8803c01e8d7b6fb1bd1b6c99cb
Sergio Silva <sergiobtos@hotmail.com>
2024-05-12 15:57:46 -0400

    perf: add promise.all to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
-import { ChevronRightIcon } from "lucide-react";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
-import ProductList from "./_components/product-list";
-import PromoBanner from "./_components/promo-banner";
 import Search from "./_components/search";
+import ProductList from "./_components/product-list";
 import { Button } from "./_components/ui/button";
+import { ChevronRightIcon } from "lucide-react";
 import { db } from "./_lib/prisma";
-import Link from "next/link";
+import PromoBanner from "./_components/promo-banner";
 import RestaurantList from "./_components/restaurant-list";
+import Link from "next/link";
 
-const Home = async () => {
-  const products = await db.product.findMany({
+const fetch = async () => {
+  const getProducts = db.product.findMany({
     where: {
       discountPercentage: {
         gt: 0,
@@ -26,6 +26,30 @@ const Home = async () => {
     },
   });
 
+  const getBurguersCategory = db.category.findFirst({
+    where: {
+      name: "Hambúrgueres",
+    },
+  });
+
+  const getPizzasCategory = db.category.findFirst({
+    where: {
+      name: "Pizzas",
+    },
+  });
+
+  const [products, burguersCategory, pizzasCategory] = await Promise.all([
+    getProducts,
+    getBurguersCategory,
+    getPizzasCategory,
+  ]);
+
+  return { products, burguersCategory, pizzasCategory };
+};
+
+const Home = async () => {
+  const { products, burguersCategory, pizzasCategory } = await fetch();
+
   return (
     <>
       <Header />
@@ -33,23 +57,26 @@ const Home = async () => {
         <Search />
       </div>
 
-      <div className="pt-6">
+      <div className="px-5 pt-6">
         <CategoryList />
       </div>
 
       <div className="px-5 pt-6">
-        <PromoBanner
-          src="/promo-banner-01.png"
-          alt="Até 30% de desconto em pizzas!"
-        />
+        <Link href={`/categories/${pizzasCategory?.id}/products`}>
+          <PromoBanner
+            src="/promo-banner-01.png"
+            alt="Até 30% de desconto em pizzas!"
+          />
+        </Link>
       </div>
 
-      <div className="space-y-3 pt-6">
+      <div className="space-y-4 pt-6">
         <div className="flex items-center justify-between px-5">
           <h2 className="font-semibold">Pedidos Recomendados</h2>
+
           <Button
-            className="h-fit p-0 text-primary hover:bg-transparent"
             variant="ghost"
+            className="h-fit p-0 text-primary hover:bg-transparent"
             asChild
           >
             <Link href="/products/recommended">
@@ -62,7 +89,12 @@ const Home = async () => {
       </div>
 
       <div className="px-5 pt-6">
-        <PromoBanner src="/promo-banner-02.png" alt="A partir de 17,90 " />
+        <Link href={`/categories/${burguersCategory?.id}/products`}>
+          <PromoBanner
+            src="/promo-banner-02.png"
+            alt="A partir de R$17,90 em lanches"
+          />
+        </Link>
       </div>
 
       <div className="space-y-4 py-6">
